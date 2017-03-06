64b495aeaaf14510c3845acc50743cb1a9eed85b
Duc Nguyen <minhducng93@gmail.com>
2017-03-06 05:45:21 -0500

    Add QR Code generator

--- generateBarCodeAndQRCode/ViewController.swift
+++ generateBarCodeAndQRCode/ViewController.swift
@@ -45,7 +45,20 @@ class ViewController: UIViewController {
         return nil
     }
     
-
+    func generateQRCode(from str: String) -> UIImage? {
+        let data = str.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let output = filter.outputImage?.applying(transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        
+        return nil
+    }
 
 }
 
