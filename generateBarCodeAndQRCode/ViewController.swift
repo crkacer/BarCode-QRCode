0d238c7f616e2fbf2203e7648766fd3046e230b3
Duc Nguyen <minhducng93@gmail.com>
2017-03-10 21:56:09 -0500

    Change the scale factor

--- generateBarCodeAndQRCode/ViewController.swift
+++ generateBarCodeAndQRCode/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         
         if let filter = CIFilter(name: "CICode128BarcodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            let transform = CGAffineTransform(scaleX: 5, y: 5)
             
             if let output = filter.outputImage?.applying(transform) {
                 return UIImage(ciImage: output)
@@ -53,7 +53,7 @@ class ViewController: UIViewController {
         
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            let transform = CGAffineTransform(scaleX: 5, y: 5)
             
             if let output = filter.outputImage?.applying(transform) {
                 return UIImage(ciImage: output)
